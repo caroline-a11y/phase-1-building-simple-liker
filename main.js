7ee16fb8119b487de0a208ad0b40462ad6d01b30
Caroline <caroline.syokau@student.moringaschool.com>
2022-12-16 05:02:34 +0300

    completed assignment

--- main.js
+++ main.js
@@ -3,7 +3,37 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const empty_heart = document.querySelectorAll('span')
 
+
+empty_heart.forEach(element => {
+
+  element.addEventListener('click', ()=>{
+
+    change_hearts()
+    mimicServerCall().catch(function(error){
+
+      document.getElementById('#modal').className = "visible"
+      change_hearts()
+
+      setTimeout(function(){ 
+        document.getElementById('#modal').className = "hidden"
+      }, 3000);
+    })
+  });
+
+  function change_hearts(){
+    if (element.className === "like-glyph"){
+
+      element.innerHTML = FULL_HEART
+      element.className = "like-glyph activated-heart"
+
+    } else if (element.className === "like-glyph activated-heart") {
+      element.innerHTML = EMPTY_HEART
+      element.className = "like-glyph"
+    }
+}
+})
 
 
 
